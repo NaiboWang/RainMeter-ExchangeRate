2e44452ad9845fa8a37c0795c967e359ad098b5a
NaiboWang-Alienware <naibowang@u.nus.edu>
2021-03-09 19:22:31 +0800

    Add Fetch Time

--- exchange/exchange/exchange/views.py
+++ exchange/exchange/exchange/views.py
@@ -1,18 +1,23 @@
 from django.http import HttpResponse
 import json,requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from threading import Timer
 
 st = ""
+lastFetchTime = ""
+lastFetchDate = ""
 
 def fetch():
-    global st
-    dt=datetime.utcnow().hour + 8
+    global st, lastFetchTime, lastFetchDate
+    dt=datetime.now().hour + 8
     if dt>9:
         url="http://web.juhe.cn:8080/finance/exchange/rmbquot?type=1&bank=&key=90f46fce44dade2b89d243980f5adfec"
         #url = "http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp"
         strhtml = requests.get(url)
         st = json.loads(strhtml.text)
+        tt = datetime.now() + timedelta(hours=8)
+        lastFetchDate = tt.strftime("%Y-%m-%d")
+        lastFetchTime = tt.strftime("%H:%M:%S")
         #print(st)
     t = Timer(900, fetch)
     t.start()
@@ -21,7 +26,7 @@ def get_time():
     return st["data"]["t"]
 
 def get_rate(i=0,t=False):
-    global st
+    global st, lastFetchTime, lastFetchDate
     if t:
         url="http://web.juhe.cn:8080/finance/exchange/rmbquot?type=1&bank=&key=90f46fce44dade2b89d243980f5adfec"
         strhtml = requests.get(url)
@@ -34,8 +39,13 @@ def get_rate(i=0,t=False):
         return st["result"][0]["港币"]["bankConversionPri"]
     elif i == 3:
         return st["result"][0]["新加坡元"]["date"]
-    else:
+    elif i == 4:
         return st["result"][0]["新加坡元"]["time"]
+    elif i == 5:
+        return lastFetchDate
+    else:
+        return lastFetchTime
+        
 
 def hello(request):
     i = int(request.GET["i"])
